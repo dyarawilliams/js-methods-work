a27fe99102769cd446511aae4696ae375407df86
D'yara Williams <dyara.williams@gmail.com>
2021-05-17 21:29:15 -0400

    added pop examples

---
+++ pop.js
@@ -0,0 +1,17 @@
+// Syntax
+// pop()
+
+// Example 1
+const str = ['Jake', 'Sam', 'Joe', 'Casey']
+str.pop()
+console.log(str) // Expected Output: ['Jake', 'Sam', 'Joe']
+
+// Example 2
+const str2 = ['potatoes', 'beans', 'corn', 'onions']
+str2.pop()
+console.log(str2) // Expected Output: ['potatoes', 'beans', 'corn']
+
+// Example 3
+const nums = [0, 11, 22, 33, 44]
+nums.pop()
+console.log(nums) // Expected Output: [0, 11, 22, 33]
